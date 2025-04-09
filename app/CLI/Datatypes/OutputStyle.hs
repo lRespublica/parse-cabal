503b2fbafe6be9446d4be259168dcaf28eb5b5c5
Leonid Znamenok <respublica@altlinux.org>
2025-04-09 17:46:29 +0300

    Updated options parser for OutputStyle
    Now maybeReader is used instead of doubtful hack with read instance

--- app/CLI/Datatypes/OutputStyle.hs
+++ app/CLI/Datatypes/OutputStyle.hs
@@ -1,44 +1,29 @@
 module CLI.Datatypes.OutputStyle (
     OutputStyle (..),
-    stringToOutputStyle,
-    outputStyles,
-    outputStylesStrings,
     outputStyleParser
  ) where
 
-import Data.List (find)
 import Data.List.Extra (enumerate)
-import Data.Maybe (listToMaybe, fromMaybe)
+import Data.Char (toLower)
 
 import Options.Applicative
 
-data OutputStyle = Plain | JSON | TOML deriving (Enum, Bounded)
-
-instance Show OutputStyle where
-    show Plain = "plain"
-    show JSON  = "json"
-    show TOML  = "toml"
-
-stringToOutputStyle :: String -> Maybe OutputStyle
-stringToOutputStyle input = find (\x -> show x == input) outputStyles
-
-instance Read OutputStyle where
-    readsPrec _ input = fromMaybe [] result
-        where
-        result :: Maybe [(OutputStyle, String)]
-        result = do
-            (readedToken, rest) <- listToMaybe . lex $ input
-            parsedStyle <- stringToOutputStyle readedToken
-            return [(parsedStyle, rest)]
+data OutputStyle = Plain | JSON | TOML deriving (Enum, Bounded, Show, Read)
 
 outputStyles :: [OutputStyle]
 outputStyles = enumerate::[OutputStyle]
 
 outputStylesStrings :: [String]
-outputStylesStrings = show <$> outputStyles
+outputStylesStrings = map ((toLower <$>) . show) outputStyles
+
+associatedOutputStyles :: [(String, OutputStyle)]
+associatedOutputStyles = zip outputStylesStrings outputStyles
+
+stringToOutputStyle :: String -> Maybe OutputStyle
+stringToOutputStyle input = lookup input associatedOutputStyles
 
 outputStyleParser :: Parser OutputStyle
-outputStyleParser = option auto
+outputStyleParser = option (maybeReader stringToOutputStyle)
    ( long "to"
   <> help "Output style"
   <> completeWith outputStylesStrings
